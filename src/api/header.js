7422a1ff306d4e44d685b1e5e9915ccfeca9d1eb
Bruceelei <1311563459@qq.com>
2019-09-11 17:12:57 +0800

    pull schedule

--- src/api/header.js
+++ src/api/header.js
@@ -2,6 +2,6 @@ export default {
   headers: {
     Accept: 'application/vnd.inee.v1+json',
     'Organ-Token': 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOjEsInN1YiI6Im9yZ2FuX2F1dGgiLCJleHAiOiIyMDE5LTA2LTA0IDEwOjA3OjA5ICswODAwIn0.Qdr-eLjk08PAGIizxPxxrE3VpS9lVO6jJ4bUJD7kczk',
-    'Auth-Token': 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOjEsInN1YiI6ImF1dGgiLCJleHAiOjE1NjgxNjkxMzR9.2_gXTMXbikjhkKULkmn3iMspfUvfxGVWEWUyDzGnq98'
+    'Auth-Token': 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOjEsInN1YiI6ImF1dGgiLCJleHAiOjE1Njg3ODc1MDN9.VNqkcQ6R7sbumq1m1X9J50PMGVPhalm2KT_ChGnA7Do'
   }
 }
