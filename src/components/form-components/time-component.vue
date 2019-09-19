aa088b28ea67cecda4096c1480daa16c9552c5b6
vergilw <antjun@126.com>
2019-09-19 10:14:56 +0800

    feat: add time component

---
+++ src/components/form-components/time-component.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="field">
+    <label class="header">{{ label }}</label>
+    <div class="horizontal">
+      <input id="start-time" type="text" placeholder="开始时间" />
+      <input id="end-time" type="text" placeholder="结束时间" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TimePicker from "timepicker.js";
+
+export default {
+  name: "TimeComponent",
+  props: {
+    name: String,
+    label: String
+  },
+  mounted: function() {
+    var timepicker = new TimePicker(["start-time", "end-time"], {
+      lang: "en",
+      theme: "blue-grey"
+    });
+    timepicker.on("change", function(evt) {
+      var value = (evt.hour || "00") + ":" + (evt.minute || "00");
+      evt.element.value = value;
+
+      console.log()
+    });
+  }
+};
+</script>
+
+<style src='timepicker.js/dist/timepicker.min.css'>
+/* global styles */
+</style> 
+
+<style scoped>
+.field > label,
+.ui.form .field > label {
+  font-size: 14px;
+  color: #4b525a;
+}
+
+.field > .horizontal,
+.ui.form .field > .horizontal {
+  display: flex;
+  justify-content: space-between;
+}
+
+.field > .horizontal > input,
+.ui.form .field > .horizontal > input {
+  width: 45%;
+  font-size: 14px;
+  color: #4b525a;
+}
+</style>
